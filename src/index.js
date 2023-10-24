1f483a1eb4846623d72c7c6d40db0ef3d65fdf75
PhamTienDat <datpham0136@gmail.com>
2023-10-24 11:38:13 -0400

    test

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <h1>MaNguonMo</h1>
+  <h1>MaNguonMo</h1>)
+  (<h2>Xin chao cac ban</h2>
 );
