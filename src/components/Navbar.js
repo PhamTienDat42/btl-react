870fbce245bac480c9a93ef44ea79b9c165f5be6
PhamTienDat <datpham0136@gmail.com>
2023-10-25 12:28:27 -0400

    add button Register and animation hideor show

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,29 @@
 import React, { useState, useEffect } from 'react';
-//import { Button } from './Button';
+import { Button } from './Button';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 
 function Navbar() {
   const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener('resize', showButton);
 
   return (
     <>
@@ -55,6 +70,7 @@ function Navbar() {
               </Link>
             </li>
           </ul>
+          {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
           </div>
         </nav>
     </>
