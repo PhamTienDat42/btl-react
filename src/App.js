d499445d98f1369e58f3dd1197c853bf650f11d1
PhamTienDat <datpham0136@gmail.com>
2023-10-25 14:47:51 -0400

    update app.js for load to products, services and signup

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Navbar from "./components/Navbar";
 import {Routes, Route, BrowserRouter} from 'react-router-dom';
 import './App.css';
 import Home from "./components/pages/Home";
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
       <Navbar />
       <Routes>
         <Route path='/' exact element={<Home />}/>
+        <Route path='/' exact element={<Products />}/>
+        <Route path='/' exact element={<Services />}/>
+        <Route path='/' exact element={<SignUp />}/>
       </Routes>
     </BrowserRouter>
     </>
